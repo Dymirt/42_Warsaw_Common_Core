3fa5678d31fb82c8d1bd185bf0f5dd2310242034
Dmytro Kolida <dkolida@c1r10s4.42warsaw.pl>
2024-03-02 19:44:21 +0100

    fixing strncmp
    Signed-off-by: Dmytro Kolida <dkolida@c1r10s4.42warsaw.pl>

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,19 +6,22 @@
 /*   By: dkolida <dkolida@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 19:28:07 by dkolida           #+#    #+#             */
-/*   Updated: 2024/02/26 15:20:37 by dkolida          ###   ########.fr       */
+/*   Updated: 2024/03/02 19:43:33 by dkolida          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
 	unsigned int	i;
+	
+	unsigned char *ptrs1 = (unsigned char *)s1;
+	unsigned char *ptrs2 = (unsigned char *)s2;
 
 	i = 0;
-	while (i < n && (s1[i] != '\0' || s2[i] != '\0'))
+	while (i < n && (ptrs1[i] != '\0' || ptrs2[i] != '\0'))
 	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+		if (ptrs1[i] != ptrs2[i])
+			return (ptrs1[i] - ptrs2[i]);
 		i++;
 	}
 	return (0);
