92e99fd21e9e3db02004e461a6cb11c92f7a97ad
Dmytro Kolida <dkolida@c1r10s4.42warsaw.pl>
2024-03-02 19:51:32 +0100

    fixing memcmp
    Signed-off-by: Dmytro Kolida <dkolida@c1r10s4.42warsaw.pl>

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,19 +6,19 @@
 /*   By: dkolida <dkolida@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/02 19:11:44 by dkolida           #+#    #+#             */
-/*   Updated: 2024/03/02 19:19:18 by dkolida          ###   ########.fr       */
+/*   Updated: 2024/03/02 19:50:53 by dkolida          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int ft_memcmp(const void *s1, const void *s2, int n)
 {
-	char *prts1 = (char *)s1;
-	char *prts2 = (char *)s2;
+	unsigned	char *prts1 = (unsigned char *)s1;
+	unsigned char *prts2 = (unsigned char *)s2;
 	
 	int	i;
 
 	i = 0;
-	while (i < n && (prts1[i] != '\0' || prts2[i] != '\0'))
+	while (i < n)
 	{
 		if (prts1[i] != prts2[i])
 			return (prts1[i] - prts2[i]);
