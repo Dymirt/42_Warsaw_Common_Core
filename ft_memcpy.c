3dbd0898bc629cd547f730c37552c29c64796b0b
Dmytro Kolida <dkolida@c3r1s2.42warsaw.pl>
2024-03-03 16:49:13 +0100

    fixing memcpy
    Signed-off-by: Dmytro Kolida <dkolida@c3r1s2.42warsaw.pl>

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,15 +6,15 @@
 /*   By: dkolida <dkolida@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/02 15:59:37 by dkolida           #+#    #+#             */
-/*   Updated: 2024/03/03 16:42:36 by dkolida          ###   ########.fr       */
+/*   Updated: 2024/03/03 16:48:56 by dkolida          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void *ft_memcpy(void *dst, const void *src, int n)
 {
 	char *ptr;
-	ptr = dst;
-	while (n > 0 && ptr && src)
+	ptr = (char *)dst;
+	while (n > 0)
 	{
 		*ptr = *(char *)src;
 
