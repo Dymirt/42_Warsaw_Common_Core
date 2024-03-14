e5661154a560e430e49372e62faba641cde26644
Dymirt <kolidadmytro@gmail.com>
2024-03-14 01:46:48 +0100

    tests for putchar
    Signed-off-by: Dymirt <kolidadmytro@gmail.com>

--- .tests/test_ft_putchar_fd.c
+++ .tests/test_ft_putchar_fd.c
@@ -10,16 +10,18 @@ test_ft_putchar_fd();
 void test_ft_putchar_fd()
 {
     //Test 1: Output to a .txt file
-    int fd1 = open("char.txt", O_WRONLY | O_CREAT);
-		if (fd1 == -1)
-			return;
+    int fd1 = open("char.txt", O_WRONLY | O_CREAT , 0666);
+	if (fd1 == -1){
+		printf("Error opening file\n");
+	}
     char c1 = 'a';
     ft_putchar_fd(c1, fd1);
     close(fd1);
 
     fd1 = open("char.txt", O_RDONLY);
-		if (fd1 == -1)
-			return;
+	if (fd1 == -1){
+		printf("Error opening file\n");
+	}
     char* result1 = (char*)malloc(5 * sizeof(char));
     int x = read(fd1, result1, 5);
     int result = c1 == result1[0];
