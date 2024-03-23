e8e8479743e46520cbb379f83a4424f1d96fd5d4
Dymirt <kolidadmytro@gmail.com>
2024-03-23 20:13:06 +0100

    moving progetc to subfolder
    Signed-off-by: Dymirt <kolidadmytro@gmail.com>

---
+++ libft/ft_strlcpy.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dkolida <dkolida@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/02 15:59:18 by dkolida           #+#    #+#             */
+/*   Updated: 2024/03/16 17:39:36 by dkolida          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	src_size;
+	size_t	i;
+
+	src_size = ft_strlen(src);
+	if (size == 0)
+		return (src_size);
+	i = 0;
+	while (i < size - 1 && src[i] != '\0')
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (src_size);
+}
