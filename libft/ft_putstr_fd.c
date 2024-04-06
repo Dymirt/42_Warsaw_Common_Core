c926a76b1b1e1c93678f937340119246b0ceb10c
Dmytro Kolida <dkolida@c3r2s3.42warsaw.pl>
2024-04-06 16:47:55 +0200

    commiting passing version libft
    Signed-off-by: Dmytro Kolida <dkolida@c3r2s3.42warsaw.pl>

--- libft/ft_putstr_fd.c
+++ libft/ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: dkolida <dkolida@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/16 17:36:14 by dkolida           #+#    #+#             */
-/*   Updated: 2024/03/16 17:58:59 by dkolida          ###   ########.fr       */
+/*   Updated: 2024/03/16 19:06:09 by dkolida          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 void	ft_putstr_fd(char *s, int fd)
 {
-	size_t len;
-	len = ft_strlen(s);
+	size_t	len;
 
+	len = ft_strlen(s);
 	if (s == NULL || fd < 0 || len == 0)
 	{
-		return;
-    }
+		return ;
+	}
 	write(fd, s, len);
 }
