5ef6926b2873362770c23f78fa6506ec587b8f92
Dmytro Kolida <dkolida@c3r2s3.42warsaw.pl>
2024-04-13 20:59:24 +0200

    fixing problem with joining memory and result counting
    Signed-off-by: Dmytro Kolida <dkolida@c3r2s3.42warsaw.pl>

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: dkolida <dkolida@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/06 21:53:15 by dkolida           #+#    #+#             */
-/*   Updated: 2024/04/07 17:33:54 by dkolida          ###   ########.fr       */
+/*   Updated: 2024/04/13 20:56:51 by dkolida          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 
 int		ft_vprintf(const char *str, va_list args);
 int		ft_swap_and_free(char **str1, char **str2);
-void	ft_printf_join(char **print_str, char *str);
+void	ft_printf_join(char **print_str, char *str, size_t print_str_len, size_t str_len);
+char *ft_memjoin(char *s1, char *s2, size_t len1, size_t len2);
+
 
 int	ft_printf(const char *str, ...)
 {
@@ -31,10 +33,13 @@ int	ft_vprintf(const char *str, va_list args)
 {
 	int		result;
 	char	*print_str;
-	char	char_to_str_tmp[2];
+	char	char_to_str_tmp[3];
 	char	*tmp;
-
-	char_to_str_tmp[1] = '\0';
+	size_t	tmp_len;
+	
+	result = 0;
+	
+	ft_bzero(char_to_str_tmp, 3);
 
 	print_str = ft_calloc(1, sizeof(char));
 	if (!print_str)
@@ -45,9 +50,19 @@ int	ft_vprintf(const char *str, va_list args)
 		{
 			str++;
 			if (*str == 'd' || *str == 'i')
-				ft_printf_join(&print_str, ft_itoa(va_arg(args, int)));
+			{
+				tmp = ft_itoa(va_arg(args, int));
+				tmp_len = ft_strlen(tmp);
+				ft_printf_join(&print_str, tmp, result, tmp_len);
+				result += tmp_len;
+			}
 			else if (*str == 'u')
-				ft_printf_join(&print_str, ft_utoa(va_arg(args, unsigned int)));
+			{
+				tmp = ft_utoa(va_arg(args, unsigned int));
+				tmp_len = ft_strlen(tmp);
+				ft_printf_join(&print_str, tmp, result, tmp_len);
+				result += tmp_len;
+			}
 			else if (*str == 's')
 			{	
 				char *arg = va_arg(args, char *);
@@ -55,13 +70,16 @@ int	ft_vprintf(const char *str, va_list args)
 					tmp = ft_strdup(arg);
 				else
 					tmp = ft_strdup("(null)");
-				ft_printf_join(&print_str, tmp);
+				tmp_len = ft_strlen(tmp);
+				ft_printf_join(&print_str, tmp, result, tmp_len);
+				result += tmp_len;
 			}		
 			else if (*str == 'c')
 			{
 				char_to_str_tmp[0] = va_arg(args, int);
-				ft_printf_join(&print_str, ft_strdup(char_to_str_tmp));
-				# TODO result is not correct for case when char is 0
+				ft_printf_join(&print_str, ft_strdup(char_to_str_tmp), result, 1);
+				ft_bzero(char_to_str_tmp, 3);
+				result++;
 			}
 			/*
 			else if (*str == '%')
@@ -77,14 +95,19 @@ int	ft_vprintf(const char *str, va_list args)
 					return (-1);
 			}*/
 			else
-				ft_printf_join(&print_str, ft_substr(str, 0, 1));
+			{
+				ft_printf_join(&print_str, ft_substr(str, 0, 1), result, 1);
+				result ++;
+			}
 		}
 		else
-			ft_printf_join(&print_str, ft_substr(str, 0, 1));
+		{
+			ft_printf_join(&print_str, ft_substr(str, 0, 1), result, 1);
+			result++;
+		}
 		str++;
 	}
-	ft_putstr_fd(print_str, 1);
-	result = ft_strlen(print_str);
+	write(1, print_str, result);
 	free(print_str);
 	return (result);
 }
@@ -100,11 +123,11 @@ int	ft_swap_and_free(char **str1, char **str2)
 	return (1);
 }
 
-void	ft_printf_join(char **print_str, char *str)
+void	ft_printf_join(char **print_str, char *str, size_t print_str_len, size_t str_len)
 {
 	char	*tmp;
 
-	tmp = ft_strjoin(*print_str, str);
+	tmp = ft_memjoin(*print_str, str, print_str_len, str_len);
 	if (!tmp)
 	{
 		free(*print_str);
@@ -113,4 +136,16 @@ void	ft_printf_join(char **print_str, char *str)
 	}
 	free(str);
 	ft_swap_and_free(print_str, &tmp);
+}
+
+char *ft_memjoin(char *s1, char *s2, size_t len1, size_t len2)
+{
+	char	*joined;
+
+	joined = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
+	if (!joined)
+		return (NULL);
+	ft_memcpy(joined, s1, len1);
+	ft_memcpy(joined + len1, s2, len2);
+	return (joined);
 }
