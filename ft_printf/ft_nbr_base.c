5705723d1c9fa1f9e27aedd48f54472dcdadeeb3
Dmytro Kolida <dkolida@c3r2s1.42warsaw.pl>
2024-04-14 13:48:22 +0200

    fixing norminette issues
    Signed-off-by: Dmytro Kolida <dkolida@c3r2s1.42warsaw.pl>

--- ft_printf/ft_nbr_base.c
+++ ft_printf/ft_nbr_base.c
@@ -6,42 +6,39 @@
 /*   By: dkolida <dkolida@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/06 21:53:07 by dkolida           #+#    #+#             */
-/*   Updated: 2024/04/14 13:28:04 by dkolida          ###   ########.fr       */
+/*   Updated: 2024/04/14 13:46:38 by dkolida          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static void	ft_nbr_base_recursive(uintptr_t nbr, char *base, size_t base_length , char **base_ptr);
+static void	ntobase_rec(uintptr_t nbr, char *base, size_t base_len, char **str);
 
-char * ft_nbr_base(uintptr_t nbr, char *base)
+char	*ft_nbr_base(uintptr_t nbr, char *base)
 {
 	size_t	base_length;
-	char	*base_ptr;
+	char	*str;
 
 	if (nbr == 0)
-    	return (ft_strdup("0"));
-
+		return (ft_strdup("0"));
 	base_length = ft_strlen(base);
-
-	base_ptr = ft_calloc(1, sizeof(char));
-
-	ft_nbr_base_recursive(nbr, base, base_length, &base_ptr);
-	return (base_ptr);
+	str = ft_calloc(1, sizeof(char));
+	ntobase_rec(nbr, base, base_length, &str);
+	return (str);
 }
 
-static void	ft_nbr_base_recursive(uintptr_t nbr, char *base, size_t base_length , char **base_ptr)
+static void	ntobase_rec(uintptr_t nbr, char *base, size_t base_len, char **str)
 {
-	char *tmp_substr;
-	char *tmp_base_ptr;
-	
-	if (nbr >= base_length)
+	char	*tmp_substr;
+	char	*tmp_base_ptr;
+
+	if (nbr >= base_len)
 	{
-		ft_nbr_base_recursive(nbr / base_length, base, base_length, base_ptr);
+		ntobase_rec(nbr / base_len, base, base_len, str);
 	}
-	tmp_substr = ft_substr(base, nbr % base_length, 1);
-	tmp_base_ptr = ft_strjoin(*base_ptr, tmp_substr);
-	free(*base_ptr);
-	*base_ptr = tmp_base_ptr;
+	tmp_substr = ft_substr(base, nbr % base_len, 1);
+	tmp_base_ptr = ft_strjoin(*str, tmp_substr);
+	free(*str);
+	*str = tmp_base_ptr;
 	free(tmp_substr);
 }
