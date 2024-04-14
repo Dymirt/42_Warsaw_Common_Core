1427beea9ad41af9ef27a60ddb013cecbf9b70b7
Dmytro Kolida <dkolida@c3r2s1.42warsaw.pl>
2024-04-14 15:03:14 +0200

    refactoring
    Signed-off-by: Dmytro Kolida <dkolida@c3r2s1.42warsaw.pl>

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,15 +6,16 @@
 /*   By: dkolida <dkolida@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/06 21:53:15 by dkolida           #+#    #+#             */
-/*   Updated: 2024/04/14 13:38:41 by dkolida          ###   ########.fr       */
+/*   Updated: 2024/04/14 15:01:41 by dkolida          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int		ft_vprintf(const char *str, va_list args);
-int		ft_swap_and_free(char **str1, char **str2);
-void	ft_printf_join(char **print_str, char *str, size_t print_str_len, size_t str_len);
+int			ft_vprintf(const char *str, va_list args);
+int			ft_swap_and_free(char **str1, char **str2);
+void		ft_printf_join(char **print_str, char *str, size_t print_str_len, size_t str_len);
+static void	handle_numbers(int *result, char **print_str, int num, char *(*f)());
 
 int	ft_printf(const char *str, ...)
 {
@@ -33,13 +34,11 @@ int	ft_vprintf(const char *str, va_list args)
 	char	*print_str;
 	char	char_to_str_tmp[3];
 	char	*tmp;
-	char 	*tmp2;
+	char	*tmp2;
 	size_t	tmp_len;
-	
-	result = 0;
-	
-	ft_bzero(char_to_str_tmp, 3);
 
+	result = 0;
+	ft_bzero(char_to_str_tmp, 3);
 	print_str = ft_calloc(1, sizeof(char));
 	if (!print_str)
 		return (-1);
@@ -50,21 +49,15 @@ int	ft_vprintf(const char *str, va_list args)
 			str++;
 			if (*str == 'd' || *str == 'i')
 			{
-				tmp = ft_itoa(va_arg(args, int));
-				tmp_len = ft_strlen(tmp);
-				ft_printf_join(&print_str, tmp, result, tmp_len);
-				result += tmp_len;
+				handle_numbers(&result, &print_str, va_arg(args, int), &ft_itoa);
 			}
 			else if (*str == 'u')
 			{
-				tmp = ft_utoa(va_arg(args, unsigned int));
-				tmp_len = ft_strlen(tmp);
-				ft_printf_join(&print_str, tmp, result, tmp_len);
-				result += tmp_len;
+				handle_numbers(&result, &print_str, va_arg(args, int), &ft_utoa);
 			}
 			else if (*str == 's')
-			{	
-				char *arg = va_arg(args, char *);
+			{
+				char	*arg = va_arg(args, char *);
 				if (arg)
 					tmp = ft_strdup(arg);
 				else
@@ -72,46 +65,32 @@ int	ft_vprintf(const char *str, va_list args)
 				tmp_len = ft_strlen(tmp);
 				ft_printf_join(&print_str, tmp, result, tmp_len);
 				result += tmp_len;
-			}		
+			}
 			else if (*str == 'c')
 			{
-				char_to_str_tmp[0] = va_arg(args, int);
-				ft_printf_join(&print_str, ft_strdup(char_to_str_tmp), result, 1);
-				ft_bzero(char_to_str_tmp, 3);
-				result++;
-			}
-			
-			else if (*str == '%')
-			{
-				char_to_str_tmp[0] = '%';
-				ft_printf_join(&print_str, ft_strdup(char_to_str_tmp), result, 1);
-				ft_bzero(char_to_str_tmp, 3);
-				result++;
+				handle_numbers(&result, &print_str, va_arg(args, int), &ft_ctoa);
 			}
 			else if (*str == 'x')
 			{
-				tmp = ft_nbr_base(va_arg(args, unsigned int), "0123456789abcdef");
+				tmp = ft_nbr_base(va_arg(args, unsigned int), BASE_16);
 				tmp_len = ft_strlen(tmp);
 				ft_printf_join(&print_str, tmp, result, tmp_len);
 				result += tmp_len;
 			}
 			else if (*str == 'X')
 			{
-				tmp = ft_nbr_base(va_arg(args, unsigned int), "0123456789ABCDEF");
+				tmp = ft_nbr_base(va_arg(args, unsigned int), BASE_16_UPPER);
 				tmp_len = ft_strlen(tmp);
 				ft_printf_join(&print_str, tmp, result, tmp_len);
 				result += tmp_len;
 			}
 			else if (*str == 'p')
 			{
-				
-				tmp2 = ft_nbr_base(va_arg(args, uintptr_t), "0123456789abcdef");
-				tmp = ft_strjoin("0x", tmp2);
-				if (tmp2[0] == '0' && tmp2[1] == '\0')
-				{
-					free(tmp);
+				tmp2 = ft_nbr_base(va_arg(args, uintptr_t), BASE_16);
+				if (tmp2[0] == '0')
 					tmp = ft_strdup("(nil)");
-				}
+				else
+					tmp = ft_strjoin("0x", tmp2);
 				free(tmp2);
 				tmp_len = ft_strlen(tmp);
 				ft_printf_join(&print_str, tmp, result, tmp_len);
@@ -159,4 +138,18 @@ void	ft_printf_join(char **print_str, char *str, size_t print_str_len, size_t st
 	}
 	free(str);
 	ft_swap_and_free(print_str, &tmp);
+}
+
+static void	handle_numbers(int *result, char **print_str, int num, char *(*f)())
+{
+	char	*tmp;
+	size_t	tmp_len;
+
+	tmp = f(num);
+	if (!num)
+		tmp_len = 1;
+	else
+		tmp_len = ft_strlen(tmp);
+	ft_printf_join(print_str, tmp, *result, tmp_len);
+	*result += tmp_len;
 }
--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: dkolida <dkolida@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/06 21:53:11 by dkolida           #+#    #+#             */
-/*   Updated: 2024/04/14 13:21:54 by dkolida          ###   ########.fr       */
+/*   Updated: 2024/04/14 14:58:16 by dkolida          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,15 @@
 
 # include "libft/libft.h"
 # include <stdarg.h>
-#include <stdint.h>
+# include <stdint.h>
+
+# define BASE_16 "0123456789abcdef"
+# define BASE_16_UPPER "0123456789ABCDEF"
 
 int		ft_printf(const char *format, ...);
 char	*ft_utoa(unsigned int n);
 char	*ft_memjoin(char *s1, char *s2, size_t len1, size_t len2);
 char	*ft_nbr_base(uintptr_t nbr, char *base);
+char	*ft_ctoa(unsigned int c);
 
 #endif
