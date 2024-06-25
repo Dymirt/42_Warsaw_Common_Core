41dc71e9e0f2e140fddd31df3433ca6b751540c7
Dymirt <kolidadmytro@gmail.com>
2024-06-25 23:50:25 +0200

    fixing ft_printf
    Signed-off-by: Dymirt <kolidadmytro@gmail.com>

--- libft/ft_printf/ft_printf.c
+++ libft/ft_printf/ft_printf.c
@@ -6,17 +6,17 @@
 /*   By: dmytrokolida <dmytrokolida@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/06 21:53:15 by dkolida           #+#    #+#             */
-/*   Updated: 2024/06/20 20:24:26 by dmytrokolid      ###   ########.fr       */
+/*   Updated: 2024/06/25 23:44:41 by dmytrokolid      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void		str_loop(const char *str, va_list args, char **p_str, size_t *res);
+void		str_loop(const char *str, va_list *args, char **p_str, size_t *res);
 
 static void	toa_join(size_t *res, char **p_str, long long num, char *(*f)());
 void		handle_p(uintptr_t nbr, size_t *res, char **p_str);
-void		match_c(const char *str, va_list args, char **p_str, size_t *res);
+void		match_c(const char *str, va_list *args, char **p_str, size_t *res);
 
 int	ft_printf(const char *str, ...)
 {
@@ -29,14 +29,14 @@ int	ft_printf(const char *str, ...)
 	if (!p_str)
 		return (-1);
 	va_start(args, str);
-	str_loop(str, args, &p_str, &result);
+	str_loop(str, &args, &p_str, &result);
 	va_end(args);
 	write(1, p_str, result);
 	free(p_str);
 	return (result);
 }
 
-void	str_loop(const char *str, va_list args, char **p_str, size_t *res)
+void	str_loop(const char *str, va_list *args, char **p_str, size_t *res)
 {
 	while (*str)
 	{
@@ -67,35 +67,35 @@ void	printf_join(char **p_str, char *str, size_t *res, size_t str_len)
 	*res += str_len;
 }
 
-static void	toa_join(size_t *res, char **p_str, long long num, char *(*f)(long long))
+void	toa_join(size_t *res, char **p_s, long long n, char *(*f)(long long))
 {
 	char	*tmp;
 	size_t	tmp_len;
 
-	tmp = f(num);
-	if (!num)
+	tmp = f(n);
+	if (tmp[0] == 0 && tmp[1] == '\0')
 		tmp_len = 1;
 	else
 		tmp_len = ft_strlen(tmp);
-	printf_join(p_str, tmp, res, tmp_len);
+	printf_join(p_s, tmp, res, tmp_len);
 }
 
-void	match_c(const char *c, va_list args, char **p_str, size_t *res)
+void	match_c(const char *c, va_list *args, char **p_str, size_t *res)
 {
 	if (*c == 'd' || *c == 'i')
-		toa_join(res, p_str, va_arg(args, int), &ft_itoa_wrapper);
+		toa_join(res, p_str, va_arg(*args, int), &ft_itoa_wrapper);
 	else if (*c == 'u')
-		toa_join(res, p_str, va_arg(args, int), &ft_utoa_wrapper);
+		toa_join(res, p_str, va_arg(*args, int), &ft_utoa_wrapper);
 	else if (*c == 's')
-		ft_printf_handle_s(va_arg(args, char *), res, p_str);
+		ft_printf_handle_s(va_arg(*args, char *), res, p_str);
 	else if (*c == 'c')
-		toa_join(res, p_str, va_arg(args, int), &ft_ctoa_wrapper);
+		toa_join(res, p_str, (va_arg(*args, int)), &ft_ctoa_wrapper);
 	else if (*c == 'x')
-		toa_join(res, p_str, va_arg(args, int), &ft_utob16_wrapper);
+		toa_join(res, p_str, va_arg(*args, int), &ft_utob16_wrapper);
 	else if (*c == 'X')
-		toa_join(res, p_str, va_arg(args, int), &ft_utob16u_wrapper);
+		toa_join(res, p_str, va_arg(*args, int), &ft_utob16u_wrapper);
 	else if (*c == 'p')
-		ft_printf_handle_p(va_arg(args, uintptr_t), res, p_str);
+		ft_printf_handle_p(va_arg(*args, uintptr_t), res, p_str);
 	else
 		printf_join(p_str, ft_substr(c, 0, 1), res, 1);
 }
--- libft/ft_printf/ft_printf_handle_p.c
+++ libft/ft_printf/ft_printf_handle_p.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf_handle_p.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dkolida <dkolida@student.42.fr>            +#+  +:+       +#+        */
+/*   By: dmytrokolida <dmytrokolida@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 17:05:58 by dkolida           #+#    #+#             */
-/*   Updated: 2024/04/14 17:06:12 by dkolida          ###   ########.fr       */
+/*   Updated: 2024/06/25 23:41:23 by dmytrokolid      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,7 @@ void	ft_printf_handle_p(uintptr_t nbr, size_t *res, char **p_str)
 	size_t	tmp_len;
 
 	tmp2 = ft_utoa_base(nbr, BASE_16);
-	if (tmp2[0] == '0')
-		tmp = ft_strdup("(nil)");
-	else
-		tmp = ft_strjoin("0x", tmp2);
+	tmp = ft_strjoin("0x", tmp2);
 	free(tmp2);
 	tmp_len = ft_strlen(tmp);
 	printf_join(p_str, tmp, res, tmp_len);
